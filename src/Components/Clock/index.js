cbf0916840be45b45966e2c9b50ca4eca09e96fa
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-07-16 10:54:12 -0500

    design complete

--- src/Components/Clock/index.js
+++ src/Components/Clock/index.js
@@ -1,31 +1,38 @@
 import './styles.css'
 import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai'
 import { MdPlayArrow, MdPause, MdReplay } from 'react-icons/md'
+import { useState } from 'react'
 
 const Clock = () => {
+	// eslint-disable-next-line no-unused-vars
+	const [segs, setSegs] = useState('00')
+	// eslint-disable-next-line no-unused-vars
+	const [mins, setMins] = useState('00')
 	return (
 		<div className='clock-container'>
 			<div className='main-tittle'>25 + 5 Clock</div>
 			<div className='clock-length'>
 				<div className='break-label'> Break Length</div>
-				<AiOutlineArrowUp style={{ width: '1rem', height: '1rem' }} />
+				<AiOutlineArrowUp className='arrow-buttons' />
 				5
-				<AiOutlineArrowDown style={{ width: '1rem', height: '1rem' }} />
+				<AiOutlineArrowDown className='arrow-buttons' />
 			</div>
 			<div className='clock-length'>
 				<div className='break-label'> Session Length</div>
-				<AiOutlineArrowUp style={{ width: '1rem', height: '1rem' }} />
+				<AiOutlineArrowUp className='arrow-buttons' />
 				5
-				<AiOutlineArrowDown style={{ width: '1rem', height: '1rem' }} />
+				<AiOutlineArrowDown className='arrow-buttons' />
 			</div>
 			<div className='screen-session'>
 				<div>Session</div>
-				<p><span id="seconds">00</span>:<span id="tens">00</span></p>
+				<p>
+					<span id='minutes'>{mins}</span>:<span id='seconds'>{segs}</span>
+				</p>
 			</div>
 			<div>
-				<MdPlayArrow />
-				<MdPause />
-				<MdReplay />
+				<MdPlayArrow className='action-buttons' />
+				<MdPause className='action-buttons' />
+				<MdReplay className='action-buttons' />
 			</div>
 			<div className='signature'>Directed by: Mario Suarez</div>
 		</div>
