b41b862ac392bc8384b19edee669844f09c6b3ea
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-08-01 18:07:29 -0500

    programing session minus

--- src/Components/Clock/index.js
+++ src/Components/Clock/index.js
@@ -2,14 +2,40 @@ import './styles.css'
 import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai'
 import { MdPlayArrow, MdPause, MdReplay } from 'react-icons/md'
 import { useState } from 'react'
-import {startTimer} from '../../startTimer'
+/* import {startTimer} from '../../startTimer' */
 
 const Clock = () => {
-	const [time, setTime] = useState({seconds: '00', minutes: '00'})
-	const [intervalId, setIntervalId] = useState('')
-	const [play, setPlay] = useState(false)
+	/* const [intervalId, setIntervalId] = useState('')
+	const [play, setPlay] = useState(false) */
+	// eslint-disable-next-line no-unused-vars
+	const [session, setSession] = useState('break')
+	const [time, setTime] = useState({minutes: '15', seconds: '00'})
+	const [clicked, setClicked] = useState(false)
+
+	const handleArrows = (timeObj, operation) => {
+		let minutes = +(timeObj.minutes)
+		if(operation === 'add'){
+			minutes += 1
+			setTime(timeObj => ({
+				...timeObj,
+				minutes: '' + minutes,
+			}))
+		}
+		if(operation === 'substract'){
+			minutes -= 1
+			setTime(timeObj => ({
+				...timeObj,
+				minutes: '' + minutes,
+			}))
+		}
+	}
+
+	const handleClickSession = () => {
+		setSession('Break')
+		setClicked(!clicked)
+	}
 	
-	const handlePlayClick = () => {
+	/* const handlePlayClick = () => {
 		if(!play) {
 			setPlay(true)
 			const Interval = setInterval(() => {
@@ -31,35 +57,35 @@ const Clock = () => {
 		clearInterval(intervalId)
 		setTime({seconds: '00', minutes: '00'})
 		setPlay(false)
-	}
+	} */
 
 	return (
 		<div className='clock-container'>
-			<div className='main-tittle'>25 + 5 Clock</div>
-			<div className='clock-length'>
+			<div className='main-tittle'>Lunch & Break Timer</div>
+			<div className={`clock-length ${clicked ? 'on-click': ""}`} onClick={handleClickSession}>
 				<div className='break-label'> Break Length</div>
-				<AiOutlineArrowUp className='arrow-buttons' />
-				5
-				<AiOutlineArrowDown className='arrow-buttons' />
+				<AiOutlineArrowUp className='arrow-buttons' onClick={() => handleArrows(time, 'add')}/>
+				{time.minutes}
+				<AiOutlineArrowDown className='arrow-buttons' onClick={() => handleArrows(time, 'substract')}/>
 			</div>
-			<div className='clock-length'>
-				<div className='break-label'> Session Length</div>
-				<AiOutlineArrowUp className='arrow-buttons' />
-				5
-				<AiOutlineArrowDown className='arrow-buttons' />
+			<div className={`clock-length ${clicked ? 'on-click': ""}`} onClick={handleClickSession}>
+				<div className='break-label'> Lunch Length</div>
+				<AiOutlineArrowUp className='arrow-buttons' onClick={() => handleArrows(time, 'add')}/>
+				{time.minutes}
+				<AiOutlineArrowDown className='arrow-buttons' onClick={() => handleArrows(time, 'substract')}/>
 			</div>
 			<div className='screen-session'>
-				<div>Session</div>
+				<div>{`${session}`}</div>
 				<p>
 					<span id='minutes'>{time.minutes}</span>:<span id='seconds'>{time.seconds}</span>
 				</p>
 			</div>
 			<div>
-				<MdPlayArrow className='action-buttons' onClick={handlePlayClick} />
-				<MdPause className='action-buttons' onClick={handlePauseClick}/>
-				<MdReplay className='action-buttons' onClick={handleResetClick}/>
+				<MdPlayArrow className='action-buttons'  />
+				<MdPause className='action-buttons' />
+				<MdReplay className='action-buttons'/>
 			</div>
-			<div className='signature'>Directed by: Mario Suarez</div>
+			<div className='signature'>Designed and coded by: <br></br>Mario Suarez</div>
 		</div>
 	)
 }
